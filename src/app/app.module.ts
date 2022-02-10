eb3511f0f6bc6b33e3ad6c5c6de844c46aeba2cf
Kaia <13533828+Lawlheart@users.noreply.github.com>
2022-02-09 16:24:41 -0800

    remove editor files, fix imports in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PetPageComponent } from './pet-page/pet-page.component';
+import { PetPageComponent } from './pages/pet-page/pet-page.component';
 import { PetHomeComponent } from './pages/pet-home/pet-home.component';
 import { PetCardComponent } from './components/pet-card/pet-card.component';
 import { PetDetailComponent } from './components/pet-detail/pet-detail.component';
